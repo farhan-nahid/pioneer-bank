2be13d019717f6e8057f87d7febcf1d3bb22cab8
farhan-nahid <nahidahmed9933@gmail.com>
2021-01-27 03:01:14 +0600

    Bank Done

--- assets/custom.js
+++ assets/custom.js
@@ -1,3 +1,5 @@
+// loginBtn Event Headers start
+
 const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click", function () {
   const loginPage = document.getElementById("login-page");
@@ -7,3 +9,51 @@ loginBtn.addEventListener("click", function () {
   const bankingArea = document.getElementById("banking-area");
   bankingArea.style.display = "block";
 });
+
+// loginBtn Event Headers end
+
+// depositBtn Event Headers start
+
+const depositBtn = document.getElementById("add-deposit");
+depositBtn.addEventListener("click", function () {
+  const depositAmount = document.getElementById("deposit-amount").value;
+  if (depositAmount === "") {
+    alert("Please Enter a Value");
+    return false;
+  }
+  const depositNumber = parseFloat(depositAmount);
+
+  updateSpanText("current-deposit", depositNumber);
+  updateSpanText("current-balance", depositNumber);
+
+  document.getElementById("deposit-amount").value = "";
+});
+
+// depositBtn Event Headers end
+
+// withdrawBtn Event Headers start
+
+const withdrawBtn = document.getElementById("add-withdraw");
+withdrawBtn.addEventListener("click", function () {
+  const withdrawAmount = document.getElementById("withdraw-amount").value;
+  if (withdrawAmount === "") {
+    alert("Please Enter a Value");
+    return false;
+  }
+  const withdrawNumber = parseFloat(withdrawAmount);
+
+  updateSpanText("current-withdraw", withdrawNumber);
+  updateSpanText("current-balance", -1 * withdrawNumber);
+
+  document.getElementById("withdraw-amount").value = "";
+});
+
+// withdrawBtn Event Headers end
+
+//function
+function updateSpanText(id, addedNumber) {
+  const current = document.getElementById(id).innerText;
+  const currentNumber = parseFloat(current);
+  const totalAmount = addedNumber + currentNumber;
+  document.getElementById(id).innerText = totalAmount;
+}
